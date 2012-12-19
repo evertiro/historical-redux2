3d8d5a852b71d24ac64c03150ee4e248143f55db
Alex Poslavsky <alexander.poslavsky@gmail.com>
2012-12-20 00:45:03 +0100

    google fonts + preview

--- admin/options/fields/google_webfonts/field_google_webfonts.php
+++ admin/options/fields/google_webfonts/field_google_webfonts.php
@@ -8,19 +8,25 @@ class Redux_Options_google_webfonts extends Redux_Options {
      *
      * @since Redux_Options 1.0.0
     */
+    // function __construct($field = array(), $value ='', $parent) {
+    //     parent::__construct($parent->sections, $parent->args, $parent->extra_tabs);
+    //     $this->field = $field;
+    //     $this->value = $value;
+    //     $this->field['fonts'] = array();
+
+    //     $fonts = get_transient('redux-opts-google-webfonts');
+    //     if(!is_array(json_decode($fonts))) {
+    //         $fonts = wp_remote_get('https://www.googleapis.com/webfonts/v1/webfonts?key=' . $this->args['google_api_key'], array('sslverify' => false));
+    //         $fonts = wp_remote_retrieve_body($fonts);
+    //         set_transient('redux-opts-google-webfonts', $fonts, 60 * 60 * 24);
+    //     }
+    //     $this->field['fonts'] = json_decode($fonts);
+    // }
     function __construct($field = array(), $value ='', $parent) {
         parent::__construct($parent->sections, $parent->args, $parent->extra_tabs);
         $this->field = $field;
         $this->value = $value;
-        $this->field['fonts'] = array();
-
-        $fonts = get_transient('redux-opts-google-webfonts');
-        if(!is_array(json_decode($fonts))) {
-            $fonts = wp_remote_get('https://www.googleapis.com/webfonts/v1/webfonts?key=' . $this->args['google_api_key'], array('sslverify' => false));
-            $fonts = wp_remote_retrieve_body($fonts);
-            set_transient('redux-opts-google-webfonts', $fonts, 60 * 60 * 24);
-        }
-        $this->field['fonts'] = json_decode($fonts);
+        //$this->render();
     }
 
     /**
@@ -31,21 +37,28 @@ class Redux_Options_google_webfonts extends Redux_Options {
      * @since Redux_Options 1.0.0
     */
     function render() {
-        $class = (isset($this->field['class'])) ? 'class="' . $this->field['class'] . '" ' : '';
-        echo '<p class="description" style="color:red;">' . __('The fonts provided below are free to use custom fonts from the <a href="http://www.google.com/webfonts" target="_blank">Google Web Fonts directory</a>.<br/>Please <a href="http://www.google.com/webfonts" target="_blank">browse the directory</a> to preview a font, then select your choice below.', Redux_TEXT_DOMAIN) . '</p>';
+        echo '<p class="description" style="color:red;">' . __('The fonts provided below are free to use custom fonts from the <a href="http://www.google.com/webfonts" target="_blank">Google Web Fonts directory</a>', Redux_TEXT_DOMAIN) . '</p>';
 
-        if(isset($this->field['fonts']->error)) {
-            echo '<p class="error"><span style="color: red;">Error:</span> ' . $this->field['fonts']->error->errors[0]->message . '</p>';
-        } else {
-            echo '<select id="' . $this->field['id'] . '" name="' . $this->args['opt_name'] . '[' . $this->field['id'] . ']" ' . $class . 'rows="6" >';
-               foreach($this->field['fonts']->items as $cut) {
-                foreach($cut->variants as $variant) {
-                    echo '<option value="' . $cut->family . ':' . $variant . '" ' . selected($this->value, $cut->family . ':' . $variant, false) . '>' . $cut->family . ' - ' . $variant . '</option>';
-                  }
-            }
-            echo '</select>';
-        }
+        echo '<input type="text" id="' . $this->field['id'] . '" name="' . $this->args['opt_name'] . '[' . $this->field['id'] . ']" class="font"  ' . $placeholder . 'value="' . esc_attr($this->value) . '" />';
 
+        echo '<h3 id="' . $this->field['id'] . '" class="example">Lorem Ipsum is simply dummy text</h3>';
         echo (isset($this->field['desc']) && !empty($this->field['desc'])) ? ' <span class="description">' . $this->field['desc'] . '</span>' : '';
+    }
+
+    /**
+     * Enqueue Function.
+     *
+     * If this field requires any scripts, or css define this function and register/enqueue the scripts/css
+     *
+     * @since Redux_Options 1.0.0
+    */
+    function enqueue() {
+        wp_enqueue_script(
+            'redux-opts-googlefonts-js', 
+            Redux_OPTIONS_URL . 'fields/google_webfonts/jquery.fontselect.js', 
+            array('jquery'),
+            time(),
+            true
+        );
     }
 }

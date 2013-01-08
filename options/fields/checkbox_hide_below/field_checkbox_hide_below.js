39a6a3a01380022b6cc125f2896864b4676e6441
Pierre Saikali <pierre@saika.li>
2013-01-08 17:47:42 -0200

    checkbox_hide_below' field: hide multiple fields at once
    Added 'next_to_hide' param to hide the X next fields.
    'next_to_hide' => 3 will hide the next 3 fields.

--- options/fields/checkbox_hide_below/field_checkbox_hide_below.js
+++ options/fields/checkbox_hide_below/field_checkbox_hide_below.js
@@ -1,13 +1,15 @@
 jQuery(document).ready(function(){
-	
+
 	jQuery('.redux-opts-checkbox-hide-below').each(function(){
+		var amount = jQuery(this).data('amount');
 		if(!jQuery(this).is(':checked')){
-			jQuery(this).closest('tr').next('tr').hide();
+			jQuery(this).closest('tr').nextAll('tr').slice(0,amount).hide();
 		}
 	});
-	
+
 	jQuery('.redux-opts-checkbox-hide-below').click(function(){
-		jQuery(this).closest('tr').next('tr').fadeToggle('slow');
+		var amount = jQuery(this).data('amount');
+		jQuery(this).closest('tr').nextAll('tr').slice(0,amount).fadeToggle('slow');
 	});
-	
+
 });
--- options/fields/checkbox_hide_below/field_checkbox_hide_below.php
+++ options/fields/checkbox_hide_below/field_checkbox_hide_below.php
@@ -24,9 +24,12 @@ class Redux_Options_checkbox_hide_below extends Redux_Options {
     */
     function render() {
         $class = (isset($this->field['class'])) ? $this->field['class'] : '';
-        echo ($this->field['desc'] != '') ? ' <label for="' . $this->field['id'] . '">' : '';
-        echo '<input type="checkbox" id="' . $this->field['id'] . '" name="' . $this->args['opt_name'] . '[' . $this->field['id'] . ']" value="1" class="' . $class . ' redux-opts-checkbox-hide-below" ' . checked($this->value, '1', false) . ' />';
-        echo (isset($this->field['desc']) && !empty($this->field['desc'])) ? ' ' . $this->field['desc'] . '</label>' : '';
+        $next_to_hide = (isset($this->field['next_to_hide'])) ? $this->field['next_to_hide'] : '1';
+
+        echo '<label for="' . $this->field['id'] . '">';
+            echo '<input data-amount="' . $next_to_hide . '" type="checkbox" id="' . $this->field['id'] . '" name="' . $this->args['opt_name'] . '[' . $this->field['id'] . ']" value="1" class="' . $class . ' redux-opts-checkbox-hide-below" ' . checked($this->value, '1', false) . ' /> ';
+            if (isset($this->field['desc']) && !empty($this->field['desc'])) echo $this->field['desc'];
+        echo '</label>';
     }
 
     /**

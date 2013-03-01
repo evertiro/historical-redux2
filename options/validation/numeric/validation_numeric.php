07c301c2d155a0ba29e560eae508524d4d07bc0e
Denis <denis0706@yandex.ru>
2013-03-01 17:51:04 +0400

    Update validation_numeric.php

--- options/validation/numeric/validation_numeric.php
+++ options/validation/numeric/validation_numeric.php
@@ -1,5 +1,5 @@
 <?php
-class Redux_Validation_numeric extends Redux_Options{	
+class Redux_Validation_numeric {	
 
 	/**
 	 * Field Constructor.
@@ -9,7 +9,6 @@ class Redux_Validation_numeric extends Redux_Options{
 	 * @since Redux_Options 1.0.0
 	*/
 	function __construct($field, $value, $current) {
-		parent::__construct();
 		$this->field = $field;
 		$this->field['msg'] = (isset($this->field['msg'])) ? $this->field['msg'] : __('You must provide a numerical value for this option.', Redux_TEXT_DOMAIN);
 		$this->value = $value;

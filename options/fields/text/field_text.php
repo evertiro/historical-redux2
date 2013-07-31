cb4d1f95e7771e871325975309c635c9b491ec09
Gregory K <gregory@tiv.net>
2013-07-31 02:16:27 -0400

    Redux_Options_text refactoring to support new HTML5 input types and attributes

--- options/fields/text/field_text.php
+++ options/fields/text/field_text.php
@@ -1,30 +1,99 @@
 <?php
+/**
+ * Render <input type="text"> and its HTML5 variations (type="number", "email", etc.)
+ * For the HTML5 input types, a special 'html5type' parameter is used.
+ * New HTML5 attributes, such as "min", "max", "step", etc., are supported
+ * @example
+ * <code>
+ *    'type'  => 'text',
+ *    'html5type' => 'number',
+ *    'min' => 0,
+ * </code>
+ */
 class Redux_Options_text {
 
-    /**
-     * Field Constructor.
-     *
-     * Required - must call the parent constructor, then assign field and value to vars, and obviously call the render field function
-     *
-     * @since Redux_Options 1.0.0
-    */
-    function __construct($field = array(), $value ='', $parent) {
-        $this->field = $field;
+	/**
+	 * Field Constructor.
+	 * @since Redux_Options 1.0.0
+	 */
+	function __construct($field = array(), $value = '', $parent) {
+		$this->field = $field;
 		$this->value = $value;
-		$this->args = $parent->args;
-    }
+		$this->args  = $parent->args;
+	}
 
-    /**
-     * Field Render Function.
-     *
-     * Takes the vars and outputs the HTML for the field in the settings
-     *
-     * @since Redux_Options 1.0.0
-    */
-    function render() {
-        $class = (isset($this->field['class'])) ? $this->field['class'] : 'regular-text';
-        $placeholder = (isset($this->field['placeholder'])) ? ' placeholder="' . esc_attr($this->field['placeholder']) . '" ' : '';
-        echo '<input type="text" id="' . $this->field['id'] . '" name="' . $this->args['opt_name'] . '[' . $this->field['id'] . ']" ' . $placeholder . 'value="' . esc_attr($this->value) . '" class="' . $class . '" />';
-        echo (isset($this->field['desc']) && !empty($this->field['desc'])) ? ' <span class="description">' . $this->field['desc'] . '</span>' : '';
-    }
+	/**
+	 * Field Render Function.
+	 * Takes the vars and outputs the HTML for the field in the settings
+	 * @since Redux_Options 1.0.0
+	 */
+	function render() {
+		/**
+		 * @url http://html5doctor.com/html5-forms-introduction-and-new-attributes/
+		 * @url https://developer.mozilla.org/en-US/docs/Web/HTML/Element/Input
+		 */
+		static $allowed_attributes = array(
+			'type',
+			'id',
+			'name',
+			'value',
+			'class',
+			'style',
+			'maxlength',
+			'placeholder',
+			'min',
+			'max',
+			'step',
+			'pattern',
+			'required',
+			'autocomplete',
+			'multiple'
+		);
+
+		/*
+		 * Do not mess with the original $this->field content
+		 */
+		$attributes = $this->field;
+
+		/*
+		 * Default input type is "text", but it can be overridden by the 'html5type' parameter
+		 */
+		$attributes['type'] = 'text';
+		if (isset($attributes['html5type'])) {
+			$attributes['type'] = $attributes['html5type'];
+			unset($attributes['html5type']);
+		}
+
+		/*
+		 * Set default class if not specified
+		 */
+		if (!isset($attributes['class'])) {
+			$attributes['class'] = 'regular-text';
+		}
+
+		/*
+		 * All form values are passed as an array, and saved as serialized
+		 */
+		$attributes['name'] = $this->args['opt_name'] . '[' . $attributes['id'] . ']';
+
+		$attributes['value'] = $this->value;
+
+		/*
+		 * Print the tag
+		 */
+		echo '<input';
+		foreach ($attributes as $name => $value) {
+			if (in_array($name, $allowed_attributes)) {
+				echo ' ' . $name . '="' . esc_attr($value) . '"';
+			}
+		}
+		echo ' />';
+
+		/*
+		 * Print the optional description
+		 */
+		if (!empty($this->field['desc'])) {
+			echo ' <span class="description">' . $this->field['desc'] . '</span>';
+		}
+	}
 }

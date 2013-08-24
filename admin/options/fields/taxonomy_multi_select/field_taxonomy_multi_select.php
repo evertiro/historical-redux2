0dd7c5c61a21fbccb6b3172f6f15337d93ed1844
Ghost1227 <dgriffiths@ghost1227.com>
2013-08-24 00:16:12 +0000

    Fixed undefined var in taxonomy multi-select

---
+++ admin/options/fields/taxonomy_multi_select/field_taxonomy_multi_select.php
@@ -0,0 +1,21 @@
+<?php
+require_once(dirname(__FILE__).'/../select/'.'field_select.php'); 
+class Redux_Options_taxonomy_multi_select extends Redux_Options_select {
+
+    /**
+     * Field Constructor.
+     *
+     * Required - must call the parent constructor, then assign field and value to vars, and obviously call the render field function
+     *
+     * @since Redux_Options 1.0.0
+    */
+    function __construct($field = array(), $value ='', $parent) {
+        $this->field = $field;
+		$this->value = $value;
+		$this->args = $parent->args;
+		
+		$wp_args = wp_parse_args($this->field['args'], array());
+		$this->field['options'] = get_taxonomies($this->field['args'], 'names');
+		$this->field['multiselect'] = true;
+    }
+}
